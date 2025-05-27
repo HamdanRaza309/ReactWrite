cab72690aaddb5966426b2c8d27c9139b5150e69
HamdanRaza309 <githubuse309@gmail.com>
2025-05-27 20:26:37 +0500

    Adds authentication handling and Redux setup
    Introduces authentication handling using authService, dispatching login/logout actions, and Redux setup with authReducer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth_service'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.currentUser()
+      .then((userData) => {
+
+        if (userData) {
+          dispatch(login(userData))
+        } else {
+          dispatch(logout())
+        }
+
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading && (
     <>
       <h1 className='text-6xl'>Hamdan Raza</h1>
+      <Header />
+      <main>
+        {/* <Outlet /> */}
+      </main>
+      <Footer />
     </>
   )
 }
